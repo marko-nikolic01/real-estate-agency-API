e48f94426652eb7abb6851c9b1c7ddd8d133ea8f
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-08-01 10:45:16 +0200

    Added a simple API GET method that returns static Property data.

--- RealEstateAgency/RealEstateAgencyAPI/Controllers/PropertyAPIController.cs
+++ RealEstateAgency/RealEstateAgencyAPI/Controllers/PropertyAPIController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateAgencyAPI.Models;
 
 namespace RealEstateAgencyAPI.Controllers
 {
     [ApiController]
+    [Route("api/Property")]
     public class PropertyAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Property> GetProperties()
+        {
+            return new List<Property> {
+                new Property { Id = 1, Name = "Beach house"},
+                new Property { Id = 2, Name = "Appartment"}
+            };
+        }
     }
 }
